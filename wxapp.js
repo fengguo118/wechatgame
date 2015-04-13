350389c097593ec1254e252c9d7cde5b5da6aa1c
fengguo118 <fengguo118@gmail.com>
2015-04-13 08:50:37 +0800

    this is update code

--- wxapp.js
+++ wxapp.js
@@ -12,5 +12,69 @@ var app     = express();
 var bodyparser = require('body-parser');
 var cookieparser = require('cookie-parser');
 var mysql    = require('mysql');
+var path     = require('path');
 
-var config  = require('')
+var config  = require('./lib/config.js').config;
+
+// console.log(config.mysql);
+
+
+/*
+ * 中间件使用
+ */
+
+ app.use(bodyparser.urlencoded({extended:false}));
+ app.use(bodyparser.json());
+ app.use(cookieparser());
+ app.use(express.static(__dirname + '/static'));
+
+/*如下是mysql的链接信息*/
+
+var mysqlConnect = mysql.createConnection(config.mysql);
+mysqlConnect.connect();
+
+var errorFunction = function(error){
+	if (!error.fatal){
+		return;
+	}
+
+	if ((error.code !== 'PROTOCOL_CONNECTION_LOST') && (error.code !== 'ECONNREFUSED')) {
+		throw error;
+	};
+
+	setTimeout(function(){
+		mysqlConnect.connect();
+	}, 1000);
+}
+
+mysqlConnect.on('error', function(error){
+	return errorFunction();
+});
+
+var keepConnected = function(){
+	setInterval(function(){
+		mysqlConnect.query("SELECT 1", function(error){
+			if (error) {
+				mysqlConnect = mysql.createConnection(config.mysql);
+				mysqlConnect.connect();
+				mysqlConnect.on('error', function(error){
+					return errorFunction(error);
+				});
+			};
+		});
+	}, 600 * 1000);
+}
+
+keepConnected();
+
+/*service api*/
+app.post('/shake', function(req, res){
+	console.log(req.body);
+    return res.send({"dat":"ks"});
+});
+
+/*启动server*/
+
+app.listen(8080);
+
+console.log("server is starting !");
